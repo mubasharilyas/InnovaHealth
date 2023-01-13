d90f3686ff07ff127fb700b13d31ec55caa72b3a
mubasharilyas <mubasharbhatti485@gmail.com>
2023-01-13 16:50:54 +0500

    dashboard fixed

--- src/app/pages/dashboard/alerts/alerts.component.ts
+++ src/app/pages/dashboard/alerts/alerts.component.ts
@@ -8,22 +8,59 @@ import { ApiService } from 'src/app/service/api.service';
   styleUrls: ['./alerts.component.scss']
 })
 export class AlertsComponent implements OnInit {
-
   page:number=1
-  alerts:any
+  limit:number=10
+  sort:number=1
+  initial_page!:number
+  total_page!:number
+  overall_total!:number
+  alerts:any=[]
     constructor(private api:ApiService) { }
   
     ngOnInit(): void {
-      this.getAlerts()
+      this.getAlerts(this.page,this.sort)
     }
   
   
   
 
-    async getAlerts(){
-      this.alerts= await lastValueFrom(this.api.get('alerts',this.page))
-      console.log("alerts",this.alerts)
+    async getAlerts(page:number,sort:number){
+      this.alerts= await lastValueFrom(this.api.getAlerts('alerts',page,sort))
+      console.log("contacts",this.alerts)
+      if(page==1){
+        this.initial_page=page,
+        this.total_page=this.alerts.results,
+        this.overall_total=this.alerts.totalCount
+
+      }
+      else if(page>1){
+        this.initial_page=((page-1)*10)+1
+        if(this.alerts.alerts.length==10){
+          this.total_page=(this.alerts.alerts.length*page)
+        }
+        else{
+          this.total_page=(this.alerts.alerts.length+((page-1)*10))
+
+        }
+        this.overall_total=this.alerts.totalCount
+      }
     }
+    sortData(sort:number){
+      console.log("sort",sort)
+      this.sort=sort
+      this.page=1
+      this.getAlerts(this.page,sort);
+
+    }
+   
+
+    alertPageChanged(event:any){
+      this.page=event
+      this.getAlerts(this.page,this.sort)
+  
+    }
+  
+
   
 
 }
--- src/app/pages/dashboard/contacts/contacts.component.ts
+++ src/app/pages/dashboard/contacts/contacts.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, lastValueFrom, Observable, Subscription, tap } from 'rxjs';
 import { ApiService } from 'src/app/service/api.service';
 
 @Component({
@@ -7,23 +7,83 @@ import { ApiService } from 'src/app/service/api.service';
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit,AfterViewInit {
+  @ViewChild('search')search!: ElementRef;
   page:number=1
-  contacts:any
+  limit:number=10
+  sort:number=1
+  contacts:any=[]
+  initial_page!:number
+  total_page!:number
+  overall_totals!:number
+  SearchSubcription!: Subscription
+  contactLoading:boolean=false
+
+
     constructor(private api:ApiService) { }
   
     ngOnInit(): void {
-      this.getContacts()
+      this.getContacts(this.page,this.sort,'')
     }
   
   
   
-    async getContacts(){
-      this.contacts= await lastValueFrom(this.api.get('contacts',this.page))
-      console.log("alerts",this.contacts)
+    async getContacts(page:number,sort:number,search:string){
+      let res = await lastValueFrom(this.api.getContact('contacts',page,sort,search))
+      this.contacts=res
+      console.log("contacts",res)
+      if(page==1){
+        this.initial_page=page,
+        this.total_page=this.contacts.results,
+        this.overall_totals=this.contacts.totalCount
 
+      }
+      else if(page>1){
+        this.initial_page=((page-1)*10)+1
+        if(this.contacts.contacts.length==10){
+          this.total_page=(this.contacts.contacts.length*page)
+        }
+        else{
+          this.total_page=(this.contacts.contacts.length+((page-1)*10))
+
+        }
+        this.overall_totals=this.contacts.totalCount
+      }
     }
    
+    ngAfterViewInit():void{
+      this.SearchSubcription=fromEvent(
+        this.search.nativeElement,
+        'keyup'
+      ).pipe(
+        filter(Boolean),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        tap(()=>{
+          this.page=1
+          this.sort=1
+          this.contactLoading=true
+          this.getContacts(this.page,this.sort,this.search.nativeElement.value);
+          setTimeout(()=>{
+            this.contactLoading=false
+          },1000)
+        })
+      ).subscribe();
+
+    }
+    sortData(sort:number){
+      console.log("sort",sort)
+      this.sort=sort
+      this.page=1
+      this.getContacts(this.page,sort,this.search.nativeElement.value);
+
+    }
+
+    pageChanged(event:any){
+      this.page=event
+      this.getContacts(this.page,this.sort,this.search.nativeElement.value)
+  
+    }
   
 
 }
